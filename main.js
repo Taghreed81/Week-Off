112c25b30fe231bd1cbf9075429317045a1dda04
Taghreed81 <tagrid21003@hotmail.com>
2020-10-10 18:39:46 +0300

    w1d2

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
+ //WEEK1 DAY1
  //EX1
-
  function myFirstFunction(){
  }
-
  //EX2
  function helloWorld(){
  	  console.log("hello world");
  }
-
-
-
  //EX3
 function returnFive(){
 	return 5;
 }
-
 //EX4	
 function helloWorldAgain(){
 	return "hello World Again";
 }
-
 //EX5
 function returnParameter(paremeter){
  return paremeter;
 }
-
 //EX6
 function add(num1, num2){
 return num1 + num2;
 }
-
 //EX7
 function subtract(num1, num2){
 return num1 - num2;
@@ -93,4 +85,63 @@ return A;
 function tripleArea(w, l) {
 var A = w * l;
 return A * 3;
+}
+
+//Week1 day 2
+//EX6
+function opposite(boolean){
+	return !boolean;
+}
+
+//EX7
+function greaterThan5(num1, num2){
+if(num1 > 5 || num2 > 5){
+	return true;
+}else{
+	return false;
+}
+
+//EX8
+function allGreaterThan5(num1, num2){
+if ( num1 && num2 > 5){
+	return true;
+}
+if (num1 && num2 < 5) {
+return false;
+}
+}
+//EX9
+function largerThan5AndLessThan20(num) {
+	if(num > 5 && num < 20){
+		return true;
+	}else{
+		return false;
+	}
+}
+//EX10
+function not6AndLessThan10(num) {
+	if( num !== 6 && num < 10){
+		return true;
+	}else {
+		return false;
+	}
+}
+//EX11
+
+function largerThan3AndLessThan18(n1, n2, n3) {
+	if ((n1 && n2 && n3 > 3) && (n1 && n2 && n3 <18)){
+		return true;
+	} else{
+		return false;
+	}
+}
+//EX12
+function cloudyAndRainy(str1, str2) {
+	var str1 = "cloudy";
+	var str2 = "rainy";
+	if(str1 && str2){
+		return true;
+	} else{
+		return false;
+	}
 }
