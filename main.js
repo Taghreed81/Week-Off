2d6941dd7aef47575f6bc62c24feecfb27116ab6
Taghreed81 <tagrid21003@hotmail.com>
2020-10-10 17:30:05 +0300

    W1

---
+++ main.js
@@ -0,0 +1,96 @@
+ //EX1
+
+ function myFirstFunction(){
+ }
+
+ //EX2
+ function helloWorld(){
+ 	  console.log("hello world");
+ }
+
+
+
+ //EX3
+function returnFive(){
+	return 5;
+}
+
+//EX4	
+function helloWorldAgain(){
+	return "hello World Again";
+}
+
+//EX5
+function returnParameter(paremeter){
+ return paremeter;
+}
+
+//EX6
+function add(num1, num2){
+return num1 + num2;
+}
+
+//EX7
+function subtract(num1, num2){
+return num1 - num2;
+}
+
+//EX8
+function multiply(num1, num2){
+return num1 * num2;
+}
+//EX9
+function stringLength(str){
+return str.length;
+}
+
+//EX10
+function concatTwoStrings(str1, str2){
+	return str1 + str2;
+}
+//EX11
+function fullName(firstName, lastName){
+	return firstName + " " + lastName;
+}
+
+function square(num){
+	return num * num;
+}
+//EX12
+function cube(num){
+return num * num * num;
+}
+//EX13
+function charAtIndex(str, num){
+	return str.charAt(num);
+}
+//EX14
+function addFourNums(n1, n2, n3, n4){
+	return n1 + n2 + n3 + n4;
+}
+//EX15
+function perimeterRect(w ,l){
+ var p = 2 * l + 2 * w;
+ return p;
+}
+//EX16
+function areaRect(w, l){
+	var A = w * l;
+	return A;
+}
+//EX17
+function perimeterTriangle(l1, l2, l3){
+	var p = l1 + l2 + l3;
+	return p;
+}
+//EX18
+function areaTraingle(b, h){
+var A = (1/2) * b * h;
+return A;
+}
+
+//EX19
+function tripleArea(w, l) {
+var A = w * l;
+return A * 3;
+}
