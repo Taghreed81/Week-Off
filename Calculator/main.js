a39048348953baa54c2dbc2b2473f702b78e5c02
Taghreed81 <tagrid21003@hotmail.com>
2020-10-12 16:45:48 +0300

    calculater -1

---
+++ Calculator/main.js
@@ -0,0 +1,23 @@
+var myNumbers = document.getElementsByClassName("give");
+var myClear   = document.getElementById("clear");
+var equal     = document.getElementById("eq");
+var myValue   = document.getElementById("type");
+ var x = myNumbers;
+
+    for ( var i = 0; i < myNumbers.length; i++) {
+    	myNumbers[i].onclick = function() {
+    		myValue.value += this.value
+    	}
+    }
+
+    myClear.onclick = function clear() {
+    	myValue.value = "";
+    }
+
+    equal.onclick = function equal() {
+    	if(myValue.value == ""){
+    		myValue.placeholder = "Enter any number";
+    	} else {
+    	myValue.value = eval(myValue.value);
+        }
+    
