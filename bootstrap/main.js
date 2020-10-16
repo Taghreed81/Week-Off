a8ad7599c42cc67bb01cb36730b156c99f6118d9
Taghreed81 <tagrid21003@hotmail.com>
2020-10-16 20:15:31 +0300

    bootstrap-1

--- bootstrap/main.js
+++ bootstrap/main.js
@@ -1,13 +1,16 @@
-// $(".btn btn-primary").click(function(){
-// 	if($("#exampleInputEmail1").val() === "" )
-// 		alret("This field must be written");
-// })
 var email = document.getElementById("exampleInputEmail1");
 var password= document.getElementById("exampleInputPassword1");
 
 function check(){
-	if(email.value.length > 9)
+	if(email.value.length === "")
 		alret('please write your email');
 }
  if(password.value.length < 7){
 		alert('Please fill up your Password');
+
+	$('.btn btn-primary').click(function(){
+	if (email.val() === x || password.val() === y ){
+		$(".signin").hide();
+		$(".next-page").show();
+	 	
+ }
